90db5081990bbc100910f9d1b4d8b8101f0598b3
Daniel <admin@myfu.net>
2014-04-30 08:12:16 -0400

    Updated - Still troubleshooting

--- src/org/starnub/network/SB_Query.java
+++ src/org/starnub/network/SB_Query.java
@@ -1,5 +1,6 @@
 package org.starnub.network;
 
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
@@ -17,18 +18,18 @@ public class SB_Query {
 
 	private static boolean serverResponse ()
 	{
-		try
-		{
-		DatagramSocket ds = new DatagramSocket(65000);
+		
 		boolean continueSending = true;
 		int counter = 0;
-	
-		//Set packet send/receive timeout (Milliseconds)
-		ds.setSoTimeout(3000);
+		DatagramSocket ds = openSocket();
 		
+		//Set packet send/receive timeout (Milliseconds)
+		try
+		{
 			// Sends packet up to 10 times if no response in 2 seconds between packets
 			while (continueSending && counter < 10) 
 			{
+				ds.setSoTimeout(3000);
 				ds.send(packetAssembly());
 				counter++;
 				try 
@@ -41,16 +42,18 @@ public class SB_Query {
 				catch (SocketTimeoutException e) 
 				{
 					// No response received after 3 second. Continue sending.
-					SN_MessageFormater.msgPrint("Starbound Query: No response from your Starbound Server", 0, 1);
+					SN_MessageFormater.msgPrint("Starbound Query: No response from the Starbound server.", 0, 1);
 				}
 			}
 			ds.close();
 		}
 		catch (Exception e)
 		{
-			SN_MessageFormater.msgPrint("Starbound Query: Error: Creating Socket or Socket Functions", 0, 1);
-			
+			SN_MessageFormater.msgPrint("Starbound Query: Error sending packets.", 0, 1);
+			return true;
 		}
+		SN_MessageFormater.msgPrint("Starbound Query: The Starbound server could not be reached.", 0, 1);
+		// Dump UDP query port, server port wrapper port
 		return false;
 	}
 	
@@ -69,13 +72,38 @@ public class SB_Query {
 			};
 		byte[] data = new String(peer0_0).getBytes();
 		// TODO import port variable
-		return new DatagramPacket(data, 0, data.length, InetAddress.getByName("127.0.0.1"), 21025);
+		return new DatagramPacket(data, 0, data.length, InetAddress.getLoopbackAddress(), 21025);
 	}
 
 	private static DatagramPacket packetReceive()
 	{
 		/* Receive portion of the UDP Query */
 		return new DatagramPacket(new byte[1024], 1024);
+	}
+	
+	private static DatagramSocket openSocket()
+	{
+		for (int port = 9000 ; port <= 65000 ; port++)
+		{
+			try 
+			{
+				return new DatagramSocket (port,InetAddress.getLoopbackAddress());
+			} 
+			catch (IOException e) 
+			{
+				// Try the next port
+				System.out.println("Port "+port+" unable to bind");
+			}
+		}
+		SN_MessageFormater.msgPrint("Starbound Query: Error: Creating Socket.", 0, 1);
+		return null;
+	}
+	
+	private static InetAddress setInetAddress()
+	{
+		//TODO check various address method
+		InetAddress.getLoopbackAddress();
+		return null;
 	}
 	
 	public SB_Query() 
