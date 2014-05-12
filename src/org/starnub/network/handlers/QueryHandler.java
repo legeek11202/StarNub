37ac75172a654093203b6a70aeeec30cf453095c
Daniel <admin@myfu.net>
2014-05-12 08:06:07 -0400

    Update QueryHandler.java
    Notation. Tabs.

--- src/org/starnub/network/handlers/QueryHandler.java
+++ src/org/starnub/network/handlers/QueryHandler.java
@@ -4,34 +4,35 @@ package org.starnub.network.handlers;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 
-/* This class is place holder for future query attempts and information */
+/* This class is place holder for future query attempts and information 
+* will need a disconnect packet and possible get play information*/
 public class QueryHandler extends ChannelInboundHandlerAdapter  {
 
-    /* Transmitting */
-    @Override
-    public void channelActive(ChannelHandlerContext ctx) throws Exception 
-    {
+    	/* Transmitting */
+   	@Override
+    	public void channelActive(ChannelHandlerContext ctx) throws Exception 
+    	{
 	}
     
-    /* Receiving */
+	 /* Receiving */
 	@Override
 	public void channelRead(final ChannelHandlerContext ctx, Object msg) throws Exception 
 	{
 	}
     
 	/* When receiving is complete */
-    @Override
-    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception 
-    {
+	 @Override
+    	public void channelReadComplete(ChannelHandlerContext ctx) throws Exception 
+    	{
 	}
 
 	@Override
 	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception 
 	{
-    }
+    	}
 	
-    
-    public void channelInactive(ChannelHandlerContext ctx) 
-    {
-    }
+	@Override
+    	public void channelInactive(ChannelHandlerContext ctx) 
+    	{
+    	}
 }
