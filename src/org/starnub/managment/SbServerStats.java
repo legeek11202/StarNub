57f9762a2c7a45b3e21d88c05a96d1d6e3fb4d8c
Daniel <admin@myfu.net>
2014-05-12 14:11:13 -0400

    Update SbServerStats.java
    Getter, Setter and Add Methods.

--- src/org/starnub/managment/SbServerStats.java
+++ src/org/starnub/managment/SbServerStats.java
@@ -3,22 +3,22 @@ package org.starnub.managment;
 public class SbServerStats {
 
 	/* Uptime Trackers */
-	static Date snOnlineTime = null; /* Plain SN Uptime date used in calculation */ //TODO get time
-	static Date sbOnlineTime = null; /* Plain SB Uptime date used in calculation */ //TODO get time
+	private static Date snOnlineTime = null; /* Plain SN Uptime date used in calculation */ //TODO get time
+	private static Date sbOnlineTime = null; /* Plain SB Uptime date used in calculation */ //TODO get time
 	
 	/* All Time Tracker */ //TODO Load from file
-	static int sbAutoRestarts = 0;
-	static int sbCrashes = 0;
-	static int sbUnresponsive = 0;
+	private static int sbAutoRestarts = 0;
+	private static int sbCrashes = 0;
+	private static int sbUnresponsive = 0;
 	
 	/* Since Wrapper Start */
-	static int sbAutoRestartsWrap = 0;
-	static int sbCrashesWrap = 0;
-	static int sbUnresponsiveWrap = 0;
+	private static int sbAutoRestartsWrap = 0;
+	private static int sbCrashesWrap = 0;
+	private static int sbUnresponsiveWrap = 0;
 	
 	/* Since SB Last Autorestart Restart */
-	static int sbCrashesTemp = 0;
-	static int sbUnresponsiveTemp = 0;
+	private static int sbCrashesTemp = 0;
+	private static int sbUnresponsiveTemp = 0;
 	
 	
 	public SbServerStats() 
@@ -35,52 +35,26 @@ public class SbServerStats {
 		
 	}
 	
-	private String getSnUptime()
-	{
-	/*Do math here to get current uptime in days, hours, minutes, seconds */
-	}
+	public String getSnUptime() { return; } //TODO Math
+	public String getSbUptime() { return; } //TODO Math
 	
-	private String getSbUptime()
-	{
-	/*Do math here to get current uptime in days, hours, minutes, seconds */
+	public int getSbAutoRestarts() { return sbAutoRestarts; }
+	public int getSbAutoRestartsWrap() { return sbAutoRestartsWrap; }
 	
-	}
+	public int getSbCrashes() { return sbCrashes; }
+	public int getSbCrashesWrap() { return sbCrashesWrap; }
+	public int getSbCrashesTemp() { return sbCrashesTemp; }
 	
-	public void setSbAutoRestarts()
-	{
-		sbAutoRestarts += 1;
-		sbAutoRestartsWrap += 1;
-	}
+	public int getSbUnresponsive() { return sbUnresponsive; }
+	public int getSbUnresponsiveWrap() { return sbUnresponsiveWrap; }
+	public int getSbUnresponsiveTemp() { return sbUnresponsiveTemp; }
 	
-	public void setSbCrashes()
-	{
-		sbCrashes += 1;
-		sbCrashesWrap += 1;
-		sbCrashesTemp += 1;
-	}
+	public void setSnOnlineTime() { snOnlineTime = DateTime(); }
+	public void setSbOnlineTime() { SbOnlineTime = DateTime(); }
 	
-	public void setSbUnresponsive()
-	{
-		sbUnresponsive += 1;
-		sbUnresponsiveWrap += 1;
-		sbUnresponsiveTemp += 1;	
-	}
+	public void addSbAutoRestarts() { sbAutoRestarts += 1; sbAutoRestartsWrap += 1; }
+	public void addSbCrashes() { sbCrashes += 1; sbCrashesWrap += 1; sbCrashesTemp += 1; }
+	public void addSbUnresponsive()	{ sbUnresponsive += 1; sbUnresponsiveWrap += 1; sbUnresponsiveTemp += 1; }
 	
-	public void setSnOnlineTime()
-	{
-		Date snOnlineTime = new DateTime();
-	}
-	
-	public void setSbOnlineTime()
-	{
-		Date sbOnlineTime = new DateTime();
-	}
-		
-	public void resetTempStats()
-	{
-		sbCrashesTemp = 0;
-	 	sbUnresponsiveTemp = 0;
-	 	Date sbOnlineTime = new DateTime();
-	}
-	
+	public void resetTempStats() { sbCrashesTemp = 0; sbUnresponsiveTemp = 0; Date sbOnlineTime = new DateTime(); }
 }
