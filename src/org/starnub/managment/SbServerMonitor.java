f94fbf7ed249a704e18c6ea2980fd2f45f621508
Daniel <admin@myfu.net>
2014-05-12 13:28:21 -0400

    Update SbServerMonitor.java
    Class additional preparation for further changes with IDE.

--- src/org/starnub/managment/SbServerMonitor.java
+++ src/org/starnub/managment/SbServerMonitor.java
@@ -39,8 +39,9 @@ public class SbServerMonitor implements Runnable {
 	//TODO Method clean up
 	private void serverMonitor ()
 	{
-		int sbCurrentUptime = 0;
 		
+		s = new SbServerStats;
+
 		processStartup();
 		
 		do 
@@ -49,12 +50,12 @@ public class SbServerMonitor implements Runnable {
 			switch (statusChecker())
 			{
 			case "statusOk" : new ThreadSleep().timer(20); sbCurrentUptime += 15; serverUptimeTemp += 15; break; // TODO Not Final //TODO Correct serverUptime format
-			case "pCrash" : serverCrashesTemp += 1; processCrashed(); break; // TODO Not Final
-			case "sUnresp" : serverUnresponsiveTemp += 1; serverUnresponsive(); break;  // TODO Not Final
+			case "pCrash" : processCrashed(); break; // TODO Not Final
+			case "sUnresp" : serverUnresponsive(); break;  // TODO Not Final
 			}			
 			if (StarNub.Debug.ON) {System.out.println("Debug: Server Monitor: Repeating Monitoring Loop.");}
 		} 
-		while (sbCurrentUptime <= autoRestartTime);
+		while (sbCurrentUptime <= autoRestartTime); //TODO Need to calculate current uptime on cycle.
 		if (StarNub.Debug.ON) {System.out.println("Debug: Server Monitor: Auto Restarting Server.");}
 
 		MessageFormater.msgPrint(s.getString("ssmar1")+" "+autoRestartTime+" "+s.getString("ssmar2"), 0, 0);
@@ -88,12 +89,13 @@ public class SbServerMonitor implements Runnable {
 		SbProcessManagment.sb_ProcessStart(); /* Start SB Server process */
 		if (StarNub.Debug.ON) {System.out.println("Debug: Server Monitor: Sleeping for 2 Minutes while the SB Server starts Up.");}
 		//TODO Convert to a response when server starts to start the monitor
-		new ThreadSleep().timer(120); /* Sleep for 2 minutes while the server boots up.*/ 
+		new ThreadSleep().timer(120); /* Sleep for 2 minutes while the server boots up.*/ //TODO Consistant server checker
 	}
 	
 	private void processCrashed ()
 	{
 		MessageFormater.msgPrint(s.getString("ssmc"), 0, 1);
+		//TODO setcrash counter and fix message
 		MessageFormater.msgPrint(s.getString("ssmc1")+" "+serverCrashesTemp+" "+s.getString("ssmc2"), 0, 1);
 		processStartup(); /* Start SB Server process */
 	}
@@ -101,7 +103,9 @@ public class SbServerMonitor implements Runnable {
 	private void serverUnresponsive ()
 	{
 		MessageFormater.msgPrint(s.getString("ssmu"), 0, 1);
+		//TODO setunresponsice counter and fix message
 		MessageFormater.msgPrint(s.getString("ssmu1")+" "+serverUnresponsiveTemp+" "+s.getString("ssmu2"), 0, 1);
-		SbProcessManagment.sb_ProcessRestart(); /* Restart's the the server monitor if the Process has Crashed */
+		SbProcessManagment.sb_ProcessKill(); /* Kill the unresponsive SB server */
+		processStartup(); /* Start up SB Server */
 	}
 }
