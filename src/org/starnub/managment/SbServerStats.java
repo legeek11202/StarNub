ec295359af8e31e1b14e66c6bf4664116d68e683
Daniel <admin@myfu.net>
2014-05-12 13:16:41 -0400

    Update SbServerStats.java
    While in training. Modified stats. more stats may be added at another date.

--- src/org/starnub/managment/SbServerStats.java
+++ src/org/starnub/managment/SbServerStats.java
@@ -2,31 +2,85 @@ package org.starnub.managment;
 
 public class SbServerStats {
 
-	// TODO push variables to menu, add all time counters
+	/* Uptime Trackers */
+	static Date snOnlineTime = null; /* Plain SN Uptime date used in calculation */ //TODO get time
+	static Date sbOnlineTime = null; /* Plain SB Uptime date used in calculation */ //TODO get time
 	
-	/* All Time */
-	static int serverAutoRestarts = 0;
-	static int serverCrashes = 0;
-	static int serverUnresponsive = 0;
+	/* All Time Tracker */ //TODO Load from file
+	static int sbAutoRestarts = 0;
+	static int sbCrashes = 0;
+	static int sbUnresponsive = 0;
 	
 	/* Since Wrapper Start */
-	static int serverAutoRestartsWrap = 0;
-	static int serverCrashesWrap = 0;
-	static int serverUnresponsiveWrap = 0;
+	static int sbAutoRestartsWrap = 0;
+	static int sbCrashesWrap = 0;
+	static int sbUnresponsiveWrap = 0;
 	
-	/* Since SN_Server Last Restart */
-	static int serverCrashesTemp = 0;
-	static int serverUnresponsiveTemp = 0;
-	static int serverUptimeTemp = 0;
+	/* Since SB Last Autorestart Restart */
+	static int sbCrashesTemp = 0;
+	static int sbUnresponsiveTemp = 0;
+	
 	
 	public SbServerStats() 
 	{
 	}
 	
-	public void getStat(){
+	private void statsLoader()
+	{
+		
+	}
+	
+	private void statsSaver()
+	{
+		
+	}
+	
+	private String getSnUptime()
+	{
+	/*Do math here to get current uptime in days, hours, minutes, seconds */
+	}
+	
+	private String getSbUptime()
+	{
+	/*Do math here to get current uptime in days, hours, minutes, seconds */
 	
 	}
 	
-	public void setStat(){
+	public void setSbAutoRestarts()
+	{
+		sbAutoRestarts += 1;
+		sbAutoRestartsWrap += 1;
 	}
+	
+	public void setSbCrashes()
+	{
+		sbCrashes += 1;
+		sbCrashesWrap += 1;
+		sbCrashesTemp += 1;
+	}
+	
+	public void setSbUnresponsive()
+	{
+		sbUnresponsive += 1;
+		sbUnresponsiveWrap += 1;
+		sbUnresponsiveTemp += 1;	
+	}
+	
+	public void setSnOnlineTime()
+	{
+		Date snOnlineTime = new DateTime();
+	}
+	
+	public void setSbOnlineTime()
+	{
+		Date sbOnlineTime = new DateTime();
+	}
+		
+	public void resetTempStats()
+	{
+		sbCrashesTemp = 0;
+	 	sbUnresponsiveTemp = 0;
+	 	Date sbOnlineTime = new DateTime();
+	}
+	
 }
