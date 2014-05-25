da50c48357e49c1536252e7e8457cfe8da9b707b
Daniel <admin@myfu.net>
2014-05-25 09:15:37 -0700

    ConnectPacketUpdate

--- src/org/starnub/network/packets/ClientConnectPacket.java
+++ src/org/starnub/network/packets/ClientConnectPacket.java
@@ -34,6 +34,119 @@ public class ClientConnectPacket extends Packet {
 	{
 	}
 
+
+	/**
+	 * @return the assetDigest
+	 */
+	public String getAssetDigest()
+	{
+		return AssetDigest;
+	}
+
+	/**
+	 * @param assetDigest the assetDigest to set
+	 */
+	public void setAssetDigest(String assetDigest)
+	{
+		AssetDigest = assetDigest;
+	}
+
+	/**
+	 * @return the claim
+	 */
+	public Variant getClaim()
+	{
+		return Claim;
+	}
+
+	/**
+	 * @param claim the claim to set
+	 */
+	public void setClaim(Variant claim)
+	{
+		Claim = claim;
+	}
+
+	/**
+	 * @return the uUID
+	 */
+	public byte[] getUUID()
+	{
+		return UUID;
+	}
+
+	/**
+	 * @param uUID the uUID to set
+	 */
+	public void setUUID(byte[] uUID)
+	{
+		UUID = uUID;
+	}
+
+	/**
+	 * @return the playerName
+	 */
+	public String getPlayerName()
+	{
+		return PlayerName;
+	}
+
+	/**
+	 * @param playerName the playerName to set
+	 */
+	public void setPlayerName(String playerName)
+	{
+		PlayerName = playerName;
+	}
+
+	/**
+	 * @return the species
+	 */
+	public String getSpecies()
+	{
+		return Species;
+	}
+
+	/**
+	 * @param species the species to set
+	 */
+	public void setSpecies(String species)
+	{
+		Species = species;
+	}
+
+	/**
+	 * @return the shipworld
+	 */
+	public byte[] getShipworld()
+	{
+		return Shipworld;
+	}
+
+	/**
+	 * @param shipworld the shipworld to set
+	 */
+	public void setShipworld(byte[] shipworld)
+	{
+		Shipworld = shipworld;
+	}
+
+	/**
+	 * @return the account
+	 */
+	public String getAccount()
+	{
+		return Account;
+	}
+
+	/**
+	 * @param account the account to set
+	 */
+	public void setAccount(String account)
+	{
+		Account = account;
+	}
+	
 	public ClientConnectPacket(String assetDigest, Variant claim, byte[] uuid, String playerName, String species, byte[] shipworld, String account)
 	{
 		AssetDigest = assetDigest;
